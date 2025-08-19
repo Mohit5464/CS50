a4215ea2d9a07a5e4c3644e5eb3bb4f659f7ed0f
Mohit5464 <128277736+Mohit5464@users.noreply.github.com>
2025-08-19 16:31:43 +0000

    Tue, Aug 19, 2025, 4:31 PM +00:00

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+#include <cs50.h>
 int main(void)
 {
-    printf("Hello world\n");
-    
+    int n = get_int("Enter the Value : \n");
+    printf("here the value %i\n", n);
 }

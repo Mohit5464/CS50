bd51a9866779baf9d41d3128edf469949b8b9f0b
Mohit5464 <128277736+Mohit5464@users.noreply.github.com>
2025-08-19 17:06:51 +0000

    Tue, Aug 19, 2025, 5:06 PM +00:00

--- hello.c
+++ hello.c
@@ -13,14 +13,21 @@ int main(void)
 {
     person people[2];
 
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-4750";
+
     string name = get_string("Name :");
     for (int i = 0 ; i < 2; i++)
     {
-        if (strcmp(names[i],name) == 0)
+        if (strcmp(people[i].name,name) == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
-    printf
+    printf("Not Found \n");
+    return 1;
 }
